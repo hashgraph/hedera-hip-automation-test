722838122fbdcc4181d6cb60a17c3293c1280749
Michael Garber <michael.garber@swirldslabs.com>
2022-09-09 09:42:54 -0400

    add arror array
    Signed-off-by: Michael Garber <michael.garber@swirldslabs.com>

--- scripts/validateHIP.js
+++ scripts/validateHIP.js
@@ -37,80 +37,79 @@ async function validate(hipPath) {
 }
 
 function validateHeaders(headers) {
+  const errmsgs = [];
   try {
-
     if (!regexs.hipNum.test(headers)) {
-      throw 'hip num must be a number use 000 if not yet assigned';
+      errmsgs.push('hip num must be a number use 000 if not yet assigned');
     }
 
     if (!regexs.title.test(headers)) {
-      throw 'header must include a title';
+      errmsgs.push('header must include a title');
     }
 
     if (!regexs.councilApproval.test(headers)) {
-      throw 'header must specify "needs-council-approval: Yes/No';
+      errmsgs.push('header must specify "needs-council-approval: Yes/No');
     }
 
     if (!regexs.status.test(headers)) {
-      throw 'header must include "status: Idea | Draft | Review | Deferred | Withdrawn | Rejected ' + 
-      '| Last Call | Council Review | Accepted | Final | Active | Replaced';
+      errmsgs.push('header must include "status: Idea | Draft | Review | Deferred | Withdrawn | Rejected ' + 
+      '| Last Call | Council Review | Accepted | Final | Active | Replaced');
     }
 
     if (!regexs.type.test(headers)) {
-      throw 'header must mast one of the following types exactly ' +
-      '"type: Standards Track | Informational | Process"';
+      errmsgs.push('header must mast one of the following types exactly ' +
+      '"type: Standards Track | Informational | Process"');
     }
 
     if (!regexs.discussions.test(headers)) {
-      throw 'header must include discussions page ' +
-      '"discussions-to: https://github.com/hashgraph/hedera-improvement-proposal/discussions/xxx"';
+      errmsgs.push('header must include discussions page ' +
+      '"discussions-to: https://github.com/hashgraph/hedera-improvement-proposal/discussions/xxx"');
     }
 
     if (/needs-council-approval: Yes/.test(headers) && /category: Application/.test(headers)) {
-      throw 'Application category HIPs do not need council approval'
+      errmsgs.push('Application category HIPs do not need council approval');
     }
 
     if (/needs-council-approval: No/.test(headers)
       && (/category: Service/.test(headers) || /category: Core/.test(headers))) {
-      throw 'Service and Core categories require council approval'
+        errmsgs.push('Service and Core categories require council approval');
     }
 
     if (!regexs.createdDate.test(headers)) {
-      throw 'created date must be in the form "created: YYYY-MM-DD';
+      errmsgs.push('created date must be in the form "created: YYYY-MM-DD');
     }
 
     if (/category:/.test(headers) && !regexs.category.test(headers)) {
-      throw 'header must match one of the following categories ' +
-      'exactly "category: Core | Service | API | Mirror | Application"';
+      errmsgs.push('header must match one of the following categories ' +
+      'exactly "category: Core | Service | API | Mirror | Application"');
     }
 
     if (/updated:/.test(headers) && !regexs.updatedDate.test(headers)) {
-      throw 'updated date must be in the form "updated: YYYY-MM-DD, YYYY-MM-DD, etc';
+      errmsgs.push('updated date must be in the form "updated: YYYY-MM-DD, YYYY-MM-DD, etc');
     }
 
     if(/last-call-date-time:/.test(headers) && ! regexs.lastCallDateTime.test(headers)) {
-      throw 'last-call-date-time should be in the form "last-call-date-time: YYYY-MM-DDTHH:MM:SSZ"';
+      errmsgs.push('last-call-date-time should be in the form "last-call-date-time: YYYY-MM-DDTHH:MM:SSZ"');
     }
 
     if (/requires:/.test(headers) && !regexs.requires.test(headers)) {
-      throw 'require field must specify the hip number(s) its referring "requires: hipnum, hipnum(s)"';
+      errmsgs.push('require field must specify the hip number(s) its referring "requires: hipnum, hipnum(s)"');
     }
 
     if (/replaces:/.test(headers) && !regexs.replaces.test(headers)) {
-      throw 'replaces field must specify the hip number(s) its referring "replaces: hipnum, hipnum(s)"';
+      errmsgs.push('replaces field must specify the hip number(s) its referring "replaces: hipnum, hipnum(s)"');
     }
 
     if (/superseded-by/.test(headers) && !regexs.supersededBy.test(headers)) {
-      throw 'superseded-by field must specify the hip number(s) its referring "superseded-by: hipnum, hipnum(s)"';
+      errmsgs.push('superseded-by field must specify the hip number(s) its referring "superseded-by: hipnum, hipnum(s)"');
+    }
+    if (errmsgs.length > 0 ) {
+      throw errmsgs
     }
   } catch (error) {
-    if (require.main === module) {
-      console.log(Error(error));
-      process.exit(1);
-    }
-    throw Error(error)
+    console.log(Error(error));
+    process.exit(1);
   }
-
 }
 
 function validateNames(line) {
